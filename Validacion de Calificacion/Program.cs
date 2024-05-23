9e36fd6cc89b323764293424f9868aa54f336883
alexm <alexm@DESKTOP-OEJ10KF>
2024-05-23 16:57:53 -0700

    Add project files.

---
+++ Validacion de Calificacion/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ValidacionClasificacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nBienvenido al Validador de Calificacion \n");
+            Console.WriteLine("\nPor favor ingrese una Calificacion: \n");
+
+            int Calificacion=int.Parse(Console.ReadLine());
+
+            if (Calificacion < 70)
+            {
+                Console.WriteLine("\nREPROBADO \n");
+
+            }
+            else if (Calificacion > 70)
+            {
+                Console.WriteLine("\nAPROVADO \n");
+            }
+            else if (Calificacion == 70)
+            {
+                Console.WriteLine("\nREGULAR \n");
+            }
+            else if (Calificacion == 85 )
+            {
+                Console.WriteLine("\nBIEN \n");
+            }
+            else if (Calificacion == 95)
+            {
+                Console.WriteLine("\nEXCELENTE \n");
+            }
+            else
+            {
+                Console.WriteLine("\nEL Valor ingresado no esta dentro de los paramentros de Validacion \n");
+            }
+
+
+
+
+        }
+    }
+}
